4945edbc7c59591ec3710459d6fb68bd15c5c812
GoomezCode <danielgdpaula@gmail.com>
2025-05-15 04:51:38 -0300

    .

---
+++ Desenvolvedor - Web/Nivel - 1/Desafio/Projetos.py
@@ -0,0 +1,43 @@
+# 1. Calculadora Simples
+# 2. jogo de adivinhação com While
+# 3. Cadastro de usuários com Input() + listas
+# 4. Mini agenda com arquivos .txt
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def desafio01():
+    operacao = input("Qual a operação da conta? | X | + | - | / | : ").lower()
+    num1 = int(input("Primeiro Num? : "))
+    num2 = int(input("Segundo Num? : "))
+
+    if operacao == "x":
+        resultado = num1 * num2
+    elif operacao == "+":
+        resultado = num1 + num2
+    elif operacao == "-":
+        resultado = num1 - num2
+    elif operacao == "/":
+        resultado = num1 / num2
+    else:
+        print("ERROR-404")
+
+    print(f"O resultado da conta é {resultado}")
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import random
+
+def desafio02():
+    numAleatorio = random.randint(1, 10)
+    while True:
+        repostaPessoa = int(input("Fala um numero de (1 a 10) : "))
+
+        if repostaPessoa == numAleatorio:
+            print("Acertou o numero")
+            break
+        elif repostaPessoa < numAleatorio:
+            print(f"O numero {repostaPessoa} e menor que o numero aleatorio")
+            print()
+        elif repostaPessoa > numAleatorio:
+            print(f"O numero {repostaPessoa} e maior que o numero aleatorio")
+            print()
+
+
--- Desenvolvedor - Web/Nivel - 1/Estudos.py
+++ Desenvolvedor - Web/Nivel - 1/Estudos.py
@@ -17,37 +17,21 @@ def c():
     print(c)
 
 # Escolher item aleatório de uma lista
-def d():
+def d(frutas):
     frutas = ["maça", "banana", "Uva", "laranja", "Melancia"]
     c = random.choice(frutas)
     print(c)
 # Embaralhar uma lista
-def e():
+def e(num):
     num = ["A", "B", "C"]
     e = random.shuffle(num)
     print(e)
 
-def f():
+def f(alunos):
     alunos = ['Ana', 'Bruno', 'Carlos', 'Duda']
     sorteados = random.sample(alunos, 2)
     print(sorteados)
+    return sorteados
 
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# while True:
-#     idade = int(input("Qual a sua idade? : "))
-
-#     if idade >= 18:
-#         print("Você tem acesso +18")
-#         break
-#     if  idade >= 12  < 18:
-#         print("Acesso Recusado -18")
-#         break
-#     else:
-#         print("Tente de novo")
-
-# while True:
-#    nome = input("Qual o seu Nome ? : ").lower()
-
-#    if nome == "":
-#        print("Coloque algum valor")s
